3f397f744dcd448b9e9c68724196548102dfd52d
brenj <brenj@mailbox.org>
2018-09-22 14:00:03 -0500

    Add get method to retrieve a block by height

--- src/app.js
+++ src/app.js
@@ -1,8 +1,36 @@
 const express = require('express');
-const Blockchain = require('private-blockchain');
+const privateBlockchain = require('private-blockchain');
 
 const app = express();
-const port = 8000;
+const blockchain = new privateBlockchain.Blockchain();
 
-app.get('/', (req, res) => res.send('Private Blockchain Web Service'));
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+const EMPTY_HEIGHT = -1;
+const PORT = 8000;
+const UNKNOWN_ERROR_MSG = 'Something bad happened ಥ_ಥ, see server logs';
+
+const convertHeightToInt = (req, res, next) => {
+  req.params.height = parseInt(req.params.height, 10);
+  next();
+};
+
+app.get('/block/:height(\\d+)', convertHeightToInt, (req, res, next) => {
+  const requestedHeight = req.params.height;
+
+  blockchain.getBlockHeight()
+    .then((lastBlockHeight) => {
+      if (lastBlockHeight === EMPTY_HEIGHT) {
+        next('ERROR: Blockchain is empty; add a block and try again.');
+      } else if (requestedHeight < 0 || requestedHeight > lastBlockHeight) {
+        next(`ERROR: Invalid block (${requestedHeight}) requested`);
+      } else {
+        return blockchain.getBlock(requestedHeight)
+          .then(block => res.send(block));
+      }
+    })
+    .catch((error) => {
+      console.error(`ERROR: ${error}`);
+      res.send(UNKNOWN_ERROR_MSG);
+    });
+});
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
